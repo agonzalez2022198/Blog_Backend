8addc63d58619478f5c677c904f418b117fa3978
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-05-14 08:26:15 -0600

    First part

--- src/comments/comments.model.js
+++ src/comments/comments.model.js
@@ -1,9 +1,11 @@
+import { Double } from "mongodb";
 import mongoose from "mongoose";
 
 const messageSchema = mongoose.Schema({
     author:{type:String},
     content: {type:String},
-    date: {type:Date, default: Date.now}
+    date: {type:Date, default: Date.now},
+    state: {type: Boolean, default: true}
 })
 
 export default mongoose.model('Message', messageSchema)
